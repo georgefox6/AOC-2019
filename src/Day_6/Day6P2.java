546e173ea58930dc1d1f609e47b4132e88780d82
GeorgeFox6 <georgefox1996@gmail.com>
2019-12-09 23:28:07 +0000

    Completed day 6 part 2

--- src/Day_6/Day6P2.java
+++ src/Day_6/Day6P2.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class Day6P2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\gfox\\Java Projects\\AOC-2019\\src\\Day_6\\input.txt"));
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\Georg\\IdeaProjects\\AOC-2019\\src\\Day_6\\input.txt"));
         String input;
         //read in all of the orbits and store them in an arraylist of lists
         ArrayList<List<String>> arr = new ArrayList<>();
@@ -34,10 +34,46 @@ public class Day6P2 {
         }
 
 
-        System.out.println(map.containsKey("SAN"));
+//        System.out.println(map.containsKey("SAN"));
         
         //Create the santa trail to the top
+        ArrayList<String> santaTrail = new ArrayList<>();
+        String santa = map.get("SAN");
+        santaTrail.add("SAN");
+        while(santa != null){
+            santaTrail.add(map.get(santa));
+            //get the parent of santa
+            santa = map.get(santa);
+        }
+
         //Create the YOU trail to the top
+        ArrayList<String> youTrail = new ArrayList<>();
+        String you = map.get("YOU");
+        youTrail.add("YOU");
+        while(you != null){
+            youTrail.add(map.get(you));
+            //get the parent of you
+            you = map.get(you);
+        }
+
+//        System.out.println("santa 0: " + santaTrail.get(0));
+//        System.out.println("santa 1: " + santaTrail.get(1));
+//        System.out.println("santa 2: " + santaTrail.get(2));
+//        System.out.println("santa 3: " + santaTrail.get(3));
+//        System.out.println("You 0: " + youTrail.get(0));
+//        System.out.println("You 1: " + youTrail.get(1));
+//        System.out.println("You 2: " + youTrail.get(2));
+//        System.out.println("You 3: " + youTrail.get(3));
+
+
         //compare the trails - find where they touch
+        for(int i=0; i<santaTrail.size(); i++){
+            for(int j=0; j<youTrail.size() - 1; j++){
+        //        System.out.println("youTrail: " + youTrail.get(j) + " SantaTrail: " + santaTrail.get(i));
+                if(youTrail.get(j).equals(santaTrail.get(i))){
+                    System.out.println("I: " + i + "J: " + j);
+                }
+            }
+        }
     }
 }
